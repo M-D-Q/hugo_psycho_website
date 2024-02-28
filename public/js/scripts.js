3721fba5ea1110888e1b9d8ab801b48cbb5704a0
M-D-Q <106432398+M-D-Q@users.noreply.github.com>
2024-02-28 16:26:58 +0100

    Delete public directory

--- public/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.therapy-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        document.querySelectorAll('.therapy-content').forEach(content => {
-            content.classList.add('d-none');
-        });
-        document.querySelector(this.getAttribute('data-bs-target')).classList.remove('d-none');
-    });
-});
-
