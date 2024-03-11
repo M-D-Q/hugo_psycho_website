dc02b3af319c6fa67d112aa1c9c9da868c33a879
M-D-Q <106432398+M-D-Q@users.noreply.github.com>
2024-03-11 23:57:28 +0100

    many good changes

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ['./layouts/**/*.html', './content/**/*.md', './themes/**/layouts/**/*.html', ],
+    content: ['./layouts/**/*.html', './content/**/*.md', './themes/**/layouts/**/*.html', './static/js/*.js'],
     css: ['./static/css/**/*.css'], // Adjust this path to where your custom CSS files are located
     output: './static/css/purged/' // Directory where the purged CSS files will be placed
   };
