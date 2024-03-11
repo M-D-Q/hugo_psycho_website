03f787c2f178067fe66872059ed11e8380acf89a
M-D-Q <106432398+M-D-Q@users.noreply.github.com>
2024-03-11 15:41:41 +0100

    added purgecss test

---
+++ purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    content: ['./layouts/**/*.html', './content/**/*.md', './themes/**/layouts/**/*.html', ],
+    css: ['./static/css/**/*.css'], // Adjust this path to where your custom CSS files are located
+    output: './static/css/purged/' // Directory where the purged CSS files will be placed
+  };
+  
